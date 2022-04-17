ed066d1ac70cb4f32f3e9e5186af8d785fdde193
Kinnick Fox <minijack4@gmail.com>
2022-04-16 19:20:37 -0500

    work complete on module_5

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,12 @@
+import os
+from pymongo import MongoClient
+
+
+url = "mongodb+srv://admin:admin@cluster0.upsow.mongodb.net/pytech"
+client = MongoClient(url)
+db = client.pytech
+print("-- Pytech COllection List --")
+for collection in db.list_collection_names():
+    print("['" + collection+ "']")
+print("End of program, press any key to exit...")
+os.system('pause > null')
